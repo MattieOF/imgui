89deef31b8c862a5860cdb3640bc6b492b738262
Matt Ware <mattbware3@gmail.com>
2022-10-30 21:43:19 +0000

    Update premake file

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-	staticruntime "off"
+	staticruntime "On"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -24,7 +24,6 @@ project "ImGui"
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-		defines { "IMGUI_API=__declspec(dllexport)" }
 
 	filter "system:linux"
 		pic "On"
